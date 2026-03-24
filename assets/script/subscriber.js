a16c81e3abae0ed33ae939cc6408df2b8c380e3e
Lateefat Obisesan <temilele06@gmail.com>
2026-03-24 16:34:10 -0500

    did my second JS

--- assets/script/subscriber.js
+++ assets/script/subscriber.js
@@ -1,1 +1,28 @@
 'use strict';
+
+import {User} from './User';
+
+class Subscriber extends User {
+    #pages;
+    #groups;
+    #canMonetize;
+
+    constructor(id, name, userName, email, pages, groups, canMonetize) {
+        super(id, name, userName, email);
+        this.#pages = pages;
+        this.#groups = groups;
+        this.#canMonetize = canMonetize;
+    }
+
+    get pages() {return this.#pages; }
+    get groups() {return this.#groups; }
+    get canMonetize() {return this.#canMonetize; }
+
+    getInfo() {
+        return `${super.getInfo()} 
+        `;
+    }
+
+}
+
+export {Subscriber};
