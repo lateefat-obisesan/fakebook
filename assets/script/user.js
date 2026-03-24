881d30cb210e72d1a8eb8e0e0b3288fec9cf32d2
Lateefat Obisesan <temilele06@gmail.com>
2026-03-24 16:22:49 -0500

    Did the first part of JS

--- assets/script/user.js
+++ assets/script/user.js
@@ -1,2 +1,31 @@
 'use strict';
 
+class User {
+    //Private properties
+    #id;
+    #name;
+    #userName;
+    #email;
+
+    //constructor
+    constructor(id, name, userName, email) {
+        this.#id = id;
+        this.#name = name;
+        this.#userName = userName;
+        this.#email = email;
+    }
+
+    //Getters
+    get id() {return this.#id; }
+    get id() {return this.#name; }
+    get id() {return this.#userName; }
+    get id() {return this.#email; }
+
+    getInfo() {
+        return ;
+    }
+
+}
+
+//now we export
+export{User}
